1ec0cf368e04f5282aa1109855502d24858fa22f
Guest User <Guest@Epicodus-10E.local>
2017-12-18 16:00:35 -0800

    make Player object and nextPlayer() function

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+const READY_STATUS = 'Ready';
+const ON_HOLD_STATUS = 'On Hold';
+
+var players = [];
+
+function Player(){
+  this.playerName = playerName;
+  this.playerSimbol = playerSimbol;//The symbol is the url to an image that represents the player.
+  this.playerCurrentPosition = currentPosition;
+  this.playerStatus;
+  this.playerId = playerId;
+}
+Player.prototype.isActive = function () {
+  return this.playerStatus === READY_STATUS;
+};
+
+function nextPlayer(players){
+  for (var i = 0; i < players.length; i++) {
+    if(players[i].isActive() && i !== players.length-1){
+      return players[i+1];
+    }
+    else {
+      return player[0];
+    }
+  }
+}
