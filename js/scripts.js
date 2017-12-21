0972803f95a9e2bfb0ed378a7ae840971f72c5a5
Guest User <Guest@Epicodus-10E.local>
2017-12-21 10:39:58 -0800

    fix frontend sripts functions

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,12 @@ const MOVE_UP_2_SPACES_BEHAVIOR = 'Move up 2 spaces';
 const GO_BACK_5_SPACES_BEHAVIOR = 'Go back 5 spaces';
 const EXTRA_TURN_BEHAVIOR = 'Extra turn';
 const NOT_BEHAVIOR = 'Not behavior';
+const PLAYERS_MAX_NUMBER = 4;
 
 //Variables
 var players = [];//Array of players
 var player;//Current player
-var player1, player2, player3, player4;
+var playersRegistrationTemp =[];
 
 var spacesNumber;//Number of spaces on the board
 var spacesOnBoard = [];//Array of spaces
@@ -146,117 +147,9 @@ function playerReady (playerReadyDivId, playerName) {
                            '</div>' +
                          '</div>');
 }
-// Grab info from playerRegistrationForm
-function playerReadyRegistrationForms () {
-  var playerOneRegistrationForm = document.getElementById('player1RegistrationForm');
-  var playerTwoRegistrationForm = document.getElementById('player2RegistrationForm');
-  var playerThreeRegistrationForm = document.getElementById('player3RegistrationForm');
-  var playerFourRegistrationForm = document.getElementById('player4RegistrationForm');
-
-  playerOneRegistrationForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-    var playerOneUserName = Capitalize($('#player1').val());
-
-    if (!playerOneUserName == "") {
-      $('#player1name').text(playerOneUserName);
-      $('#player1RegistrationForm').hide();
-      playerReady('#player1Ready', playerOneUserName);
-      pimg = "<img class='playerImg' src='" + $('#playerOneImg').attr('src') + "'>";
-      player1 = new Player(playerOneUserName, pimg, 0, READY_STATUS, 1);
-      players.push(player1);
-      player = player1;
-      spacesOnBoard[0].spacePlayers.push(player1);
-      $('#0').find('.playerDeck').append('<div class="playerCard">' + players[0].playerSimbol+" " + '</div>');
-    }
-  });
-
-  playerTwoRegistrationForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-    var playerTwoUserName = Capitalize($('#player2').val());
-
-    if (!playerTwoUserName == "") {
-      $('#player2name').text(playerTwoUserName);
-      $('#player2RegistrationForm').hide();
-      playerReady('#player2Ready', playerTwoUserName);
-      pimg = "<img class='playerImg' src='" + $('#playerTwoImg').attr('src') + "'>";
-      player2 = new Player(playerTwoUserName, pimg, 0, ON_HOLD_STATUS, 2);
-      players.push(player2);
-      spacesOnBoard[0].spacePlayers.push(player2);
-      $('#0').find('.playerDeck').append('<div class="playerCard">' + players[1].playerSimbol+" " + '</div>');
-    }
-  });
-
-  playerThreeRegistrationForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-    var playerThreeUserName = Capitalize($('#player3').val());
-
-    if (!playerThreeUserName == "") {
-      $('#player3name').text(playerThreeUserName);
-      $('#player3RegistrationForm').hide();
-      playerReady('#player3Ready', playerThreeUserName);
-      pimg = "<img class='playerImg' src='" + $('#playerThreeImg').attr('src') + "'>";
-      player3 = new Player(playerThreeUserName, pimg, 0, ON_HOLD_STATUS, 3);
-      players.push(player3);
-      spacesOnBoard[0].spacePlayers.push(player3);
-      $('#0').find('.playerDeck').append('<div class="playerCard">' + players[2].playerSimbol+" " + '</div>');
-
-    }
-  });
-
-  playerFourRegistrationForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-    var playerFourUserName = Capitalize($('#player4').val());
-    if (!playerFourUserName == "") {
-      $('#player4name').text(playerFourUserName);
-      $('#player4RegistrationForm').hide();
-      playerReady($('#player4Ready'), playerFourUserName);
-      pimg = "<img class='playerImg' src='" + $('#playerFourImg').attr('src') + "'>";
-      player4 = new Player(playerFourUserName, pimg, 0, ON_HOLD_STATUS, 4);
-      // console.log(players);
-      players.push(player4);
-      spacesOnBoard[0].spacePlayers.push(player4);
-      $('#0').find('.playerDeck').append('<div class="playerCard">' + players[3].playerSimbol+" " + '</div>');
-    }
-  });
-}
-
-// Document Ready Function
-var roll = function() {
-  return Math.floor((Math.random() * 6) + 1);
-}
-
 $(function(){
-  // player1 = new Player("player1", "simbol1", 0, READY_STATUS, 1);
-  // player2 = new Player("player2", "simbol2", 0, ON_HOLD_STATUS, 2);
-  // player3 = new Player("player3", "simbol3", 0, ON_HOLD_STATUS, 3);
-  // players.push(player1);
-  // players.push(player2);
-  // players.push(player3);
-  // player = player1;
-  // $("form.diceroll").submit(function(event) {
-  //   event.preventDefault();
-  // $("span#rollNumber").text(roll);
-
-  // $("form.diceroll").submit(function(event) {
-  //   event.preventDefault();
-  //   // debugger;
-  //   $("span#rollNumber").text(roll);
-  // }
-
-  playerReadyRegistrationForms();
-  $('.span-pg').click(function () {
-    $('.gameboard').removeClass('hidden');
-    $('.span-pg').addClass('hidden');
-    $('.nobg').addClass('hidden');
-  });
-
   spacesNumber = 14;
   var space;
-
   for (var i = 0; i < spacesNumber; i++) {
     switch (i) {
       case 2:
@@ -282,11 +175,31 @@ $(function(){
   for (var i = 0; i < spacesOnBoard.length; i++) {
       $('#'+i).append('<div class="playerDeck"></div');
   }
-
-  // for (var i = 0; i < players.length; i++) {
-  //   $('#0').append(players[i].playerSimbol+" ")
-  // }
-
+  $('.registerButtons').click(function(event){
+    event.preventDefault();
+    var idButton = $(this).attr("id");
+    if ($('#playerName'+idButton).val() !== "") {
+      $('#playerNavName'+idButton).text($('#playerName'+idButton).val());
+      $('#playerRegistration'+idButton).hide();
+      playerReady('#playerReady'+idButton, $('#playerName'+idButton).val());
+    }
+  })
+  $('#playerRegistrationForm').submit(function(event){
+    event.preventDefault();
+    for (var i = 1; i <= PLAYERS_MAX_NUMBER; i++) {
+      if ($('#playerName'+i).val() !== "") {
+        var playerName = $('#playerName'+i).val();
+        var playerSimbol = $('#playerSimbol'+i).val();
+        var playerRegistration = new Player(playerName, playerSimbol, spacesOnBoard[0].spaceNumber, ON_HOLD_STATUS, i);
+        players.push(playerRegistration);
+        spacesOnBoard[0].spacePlayers.push(playerRegistration);
+        $('#'+spacesOnBoard[0].spaceNumber).find('.playerDeck').append('<div class="playerCard">' +playerRegistration.playerSimbol+" " + '</div>');
+      }
+    }
+    $('.gameboard').removeClass('hidden');
+    $('.span-pg').addClass('hidden');
+    $('.nobg').addClass('hidden');
+  })
   $('#test').click(function(event){
     event.preventDefault();
     var diceValue = throwDice();
